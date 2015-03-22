2d85ce3967efc1c7d01623187ad36e33635a7db9
lalbarakat <saasbook@saasbook.(none)>
2015-03-22 20:51:00 -0300

    add cache to node

--- src/include/node.h
+++ src/include/node.h
@@ -1,8 +1,34 @@
-#include "Communication.h"
+#ifndef NODE_H
+#define NODE_H
 
-class Node : communication
-{
+#include <cmath>
+#include <iostream>
 
+typedef unsigned ulong;
+typedef unsigned uchar;
+typedef unsigned uint;
+
+enum {
+	INVALID = 0,
+	VALID,
+	DIRTY
+};
+	
+class cacheLine {
+protected:
+	ulong tag;
+	ulong Flags;
+	ulong seq;
+public:
+	cacheLine()	{tag = 0; Flags = 0;}
+};
+
+class Cache {
+protected:
+	ulong size, lineSize;
+	cacheLine **cache;
 };
 
 
+
+#endif
